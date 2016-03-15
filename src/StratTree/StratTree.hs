613bb3589a80f529fecae2bc93d50c4791014a12
marklnichols <marklnichols@acm.org>
2016-03-15 09:47:56 -0400

    Bug fix
    Bug fix for random choices

--- src/StratTree/StratTree.hs
+++ src/StratTree/StratTree.hs
@@ -20,7 +20,6 @@ best tree depth color =
         headM = pathM >>= headMay  
         followingM = pathM >>= tailMay 
         randChoiceM = (liftM2 (:)) headM (Just rChoices)
-       
         --TODO: implement randchoices with different scores once scoreTolerance is added -- for now they are the same
         f bestScore xs = fmap (\x -> MoveScore x bestScore) xs
         scoresM = liftM (f bestScore) randChoiceM
@@ -49,24 +48,15 @@ findBest (Node n []) depth color = ([getMove n], [], color * getValue n)
 findBest (Node n xs) 0 color = ([getMove n], [], color * getValue n)
 findBest (Node n xs) depth color = 
     let (bestMvs, randChoices,  bestVal) = foldl (bestFold depth color) ([], [], minBound) xs
-    --todo: for each randChoice need to build a list of moves/scores similar to what
-    -- we are doing for moves now -- so that for each rand choice we have the path 
-    -- containing both moves and scores -- we can get rid of a "main" move path
-    --i.e. [[rndchoice1MvPath], [rndChoice2MvPath], etc.]]
-    --findBest can now just return :: [[MoveScore]]
-    --and we can probably get rid of the Result type
     in (getMove n : bestMvs, randChoices, bestVal)
 
         
 --bestFold :: depth -> color -> ([best move list], [equiv random choices], best score) 
 bestFold :: TreeNode t => Int -> Int -> ([Int], [Int], Int) -> Tree t -> ([Int], [Int], Int)
 bestFold depth color (rMvs, randChoices, rVal) t = 
-    --todo: treat randChoices the same way as mvs -- using rXYZ and newXYZ
-    --in the LT case below, the previous randChoices need to be disgarded...
     let (mvs, _, v) = findBest t (depth - 1) (-color)
         (newMvs, newVal) = (mvs, -v)
     in  case rVal `compare` newVal of 
-    --TODO: EQ -> rMvs, newMvs : randChoices_xxs - adding list to [[]]
         EQ -> (rMvs, head newMvs : randChoices, rVal)
-        LT -> (newMvs, randChoices, newVal)
+        LT -> (newMvs, [], newVal)
         GT ->  (rMvs, randChoices, rVal)
