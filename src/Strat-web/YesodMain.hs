08a30a38442f7dab80da9624bb7a429415dec773
marklnichols <marklnichols@gmail.com>
2017-02-05 18:24:39 -0500

    yet more Yesod changes

--- src/Strat-web/YesodMain.hs
+++ src/Strat-web/YesodMain.hs
@@ -1,13 +1,15 @@
-{-# LANGUAGE QuasiQuotes, TemplateHaskell, OverloadedStrings, TypeFamilies, MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, OverloadedStrings, TypeFamilies, 
+    MultiParamTypeClasses, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall -fno-warn-missing-signatures -fno-warn-unused-binds #-}
 module YesodMain (webInit) where
 
 import Yesod hiding (insert)
 import Yesod.Static
-import Checkers as CK
+import Checkers
+import CheckersJson
 import WebRunner
 import Data.Text (Text)
-import Data.Aeson (Value)
+import Data.Aeson
 import Data.Maybe
 import Data.IORef
 import qualified Data.Map.Strict as M
@@ -17,15 +19,16 @@ staticFilesList "src/Strat-web/Static" ["gameboard.html"] -- , "image.png"]
 
 data GameApp = GameApp { getStatic :: Static, 
                          getCounter :: IORef Integer, 
-                         getMap :: IORef (M.Map Text (Tree CK.CkNode)) }
+                         getMap :: IORef (M.Map Text (Tree CkNode)) }
 
-emptyMap :: M.Map Text (Tree CK.CkNode)
+emptyMap :: M.Map Text (Tree CkNode)
 emptyMap = M.empty                                     
                                      
 mkYesod "GameApp" [parseRoutes|
 / RootR GET
 /new NewGameR GET
 /computerMove ComputerMoveR GET
+/playerMove PlayerMoveR POST
 /count CounterR GET
 /static StaticR Static getStatic
 |]
@@ -42,7 +45,7 @@ getCounterR = do
 incCount :: Num a => IORef a -> IO a
 incCount counter = atomicModifyIORef counter (\c -> (c+1, c))
 
-updateMap :: IORef (M.Map Text (Tree CK.CkNode)) -> Text -> Tree CK.CkNode -> IO ()
+updateMap :: IORef (M.Map Text (Tree CkNode)) -> Text -> Tree CkNode -> IO ()
 updateMap mapRef key value = do 
     theMap <- readIORef mapRef
     writeIORef mapRef (M.insert key value theMap)
@@ -82,7 +85,7 @@ getNewGameR :: Handler Value
 getNewGameR = do
     liftIO $ putStrLn "incoming new game request"
     uniqueId <- getGameSession
-    wrapper <- liftIO $ processStartGame CK.getStartNode True
+    wrapper <- liftIO $ processStartGame getStartNode True
     let node = getNode wrapper
     let jAble = getJsonable wrapper
     yesod <- getYesod
@@ -97,16 +100,48 @@ getComputerMoveR = do
     yesod <- getYesod
     theMap <- liftIO $ readIORef $ getMap yesod
     wrapper <- liftIO $ case M.lookup uniqueId theMap of
-                            Nothing -> do 
-                                liftIO $ putStrLn "getComputerMoveR - no tree found in the map"
-                                processError "Something is wrong with this game" CK.getStartNode
-                            Just t -> processComputerMove t 
+        Nothing -> do 
+            liftIO $ putStrLn "getComputerMoveR - no tree found in the map"
+            processError "Something is wrong with this game" getStartNode
+        Just t -> processComputerMove t 
     let node = getNode wrapper
     liftIO $ updateMap (getMap yesod) uniqueId node
     let jAble = getJsonable wrapper
     case jAble of
         Jsonable j -> returnJson j
-  
+
+--TODO: factor out some common code        
+postPlayerMoveR :: Handler Value
+postPlayerMoveR = do
+    liftIO $ putStrLn "incoming computer player move"
+    (resultM :: Result JsonMove) <- parseJsonBody 
+    yesod <- getYesod
+    theMap <- liftIO $ readIORef $ getMap yesod
+    uniqueId <- getGameSession
+    wrapper <- liftIO $ case M.lookup uniqueId theMap of
+        Nothing -> do 
+            liftIO $ putStrLn "getPlayerMoveR - no tree found in the map"
+            processError "Something is wrong with this game" getStartNode
+        Just tree -> 
+            --TODO: clean up these nested cases after this is working...      
+            case resultM of
+                Error e -> do
+                    liftIO $ putStrLn "getPlayerMoveR - could not parse the json from the client"
+                    liftIO $ putStrLn ("Error retuned: " ++ e)
+                    processError "Something is wrong with this game" getStartNode
+                Success jMove ->   
+                    case jsonMoveToCkMove jMove of
+                        Nothing -> do
+                            liftIO $ putStrLn "getPlayerMoveR - could not covert to CkMove"
+                            processError "Something is wrong with this game" getStartNode
+                        Just move -> 
+                            processPlayerMove tree move True  --true, computer always responds -- for now     
+    let node = getNode wrapper 
+    liftIO $ updateMap (getMap yesod) uniqueId node
+    let jAble = getJsonable wrapper
+    case jAble of
+        Jsonable j -> returnJson j
+        
 webInit :: IO ()
 webInit = do
     counter <- newIORef 0
