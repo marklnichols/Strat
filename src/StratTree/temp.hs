116312e12a2587422264a9292a3cdc4077c7f96c
marklnichols <marklnichols@gmail.com>
2017-07-22 11:05:57 -0400

    Added project readme

--- src/StratTree/temp.hs
+++
@@ -1,8 +0,0 @@
-
-   -- isLegal :: PositionNode n m => Tree n -> m -> Bool
-   -- parseCkMove :: CkNode -> String -> Either String CkMove
-    
-    
-let n = nodeFromGridW board06
-
-let eit = parseCkMove n "F4 G3" 
